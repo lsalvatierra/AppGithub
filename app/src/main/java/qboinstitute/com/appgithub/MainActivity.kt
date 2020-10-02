c40af234dc9ef05a59be57b275e2169e6232daeb
LuisAngel <luisangel.salvat@gmail.com>
2020-10-01 21:43:11 -0500

    Initial commit

---
+++ app/src/main/java/qboinstitute/com/appgithub/MainActivity.kt
@@ -0,0 +1,11 @@
+package qboinstitute.com.appgithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
